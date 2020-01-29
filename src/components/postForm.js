afa9dc153d6e28d9b7bd8153ae07e0aaf3b9e073
Wei Zhang <sheepchina@hotmail.com>
2020-01-29 10:37:06 -0600

    Coding for postForm.js file

---
+++ src/components/postForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import './postForm.css'
+
+class postForm extends Component {
+    state = {
+        content: ""
+    }
+    componentDidMount () {
+
+    }
+    onChange (value) {
+        this.setState({
+            content: value
+        });
+    }
+    onClick () {
+        this.setState({ content: "" });
+        this.props.createPost(this.state.content)
+    }
+    render () {
+        // let { content } = this.state
+        return (<div>
+            <div className="form">
+                <div className="title"><label htmlFor="post">Create post</label></div>
+                <br />
+                <div>
+                    <img className="avatar" src={this.props.user.avatar} alt="" />
+                    <textarea id="post" value={this.state.content} onChange={e => this.onChange(e.target.value)} placeholder="what's no your mind?" />
+                </div>
+                <button className="btn" onClick={() => { this.onClick() }}>Post</button>
+            </div>
+        </div>);
+    }
+}
+
+export default postForm;
