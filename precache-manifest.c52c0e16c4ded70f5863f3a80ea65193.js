e8f376f7e3f3f67d8d2ffb427bf8a01f30074612
Wei Zhang <sheepchina@hotmail.com>
2020-01-29 11:45:32 -0600

    Updates

---
+++ precache-manifest.c52c0e16c4ded70f5863f3a80ea65193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51858f4507af6e3653ae2994654b0651",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c63eba7d8d7bf58e9c5",
+    "url": "./static/css/main.05645487.chunk.css"
+  },
+  {
+    "revision": "2b1369fae9d5fc6139b2",
+    "url": "./static/js/2.7f30aaac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.7f30aaac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c63eba7d8d7bf58e9c5",
+    "url": "./static/js/main.70abd2f1.chunk.js"
+  },
+  {
+    "revision": "0b7968324ae2da950142",
+    "url": "./static/js/runtime-main.f8ba1d98.js"
+  }
+]);
