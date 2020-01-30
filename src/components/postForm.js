3a6e94012fabfa810827ca4127765ce10eb7de31
Wei Zhang <sheepchina@hotmail.com>
2020-01-30 14:31:13 -0600

    Add some comments to postForm.js

--- src/components/postForm.js
+++ src/components/postForm.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 
 import './postForm.css'
 
+//This part is for grabbing content from user and put it into commment
+//I set up a state here to store the content temporarily and put this content to Post component through funtion createPost
+//to create a Post component, that means the comment
+
 class postForm extends Component {
   state = {
-      content: ""
+    content: ""
   }
   componentDidMount () {
 
   }
   onChange (value) {
-      this.setState({
-          content: value
-      });
+    this.setState({
+      content: value
+    });
   }
   onClick () {
       this.setState({ content: "" });
@@ -20,17 +24,18 @@ class postForm extends Component {
   }
   render () {
       // let { content } = this.state
-      return (<div>
-          <div className="form">
-              <div className="title"><label htmlFor="post">Create post</label></div>
-              <br />
-              <div>
-                  <img className="avatar" src={this.props.user.avatar} alt="" />
-                  <textarea id="post" value={this.state.content} onChange={e => this.onChange(e.target.value)} placeholder="what's on your mind?" />
-              </div>
-              <button className="btn" onClick={() => { this.onClick() }}>Post</button>
-          </div>
-      </div>);
+    return (
+    <div>
+      <div className="form">
+        <div className="title"><label htmlFor="post">Create post</label></div>
+          <br />
+            <div>
+              <img className="avatar" src={this.props.user.avatar} alt="" />
+              <textarea id="post" value={this.state.content} onChange={e => this.onChange(e.target.value)} placeholder="what's on your mind?" />
+            </div>
+          <button className="btn" onClick={() => { this.onClick() }}>Post</button>
+      </div>
+    </div>);
   }
 }
 
